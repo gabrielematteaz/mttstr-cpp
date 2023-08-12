7e15f48c9a7be89b5fbd0cfe495884eefc3e2ef2
Matteaz <79192782+gabrielematteaz@users.noreply.github.com>
2023-08-12 15:07:17 +0200

    added "std::" to size_t

--- mttstr.hpp
+++ mttstr.hpp
@@ -5,7 +5,7 @@
 #include <cstdint>
 #include <climits>
 
-void *mttstr_mem_rev(void *mem, size_t n);
+void *mttstr_mem_rev(void *mem, std::size_t n);
 
 #define FMT_FLAGS_UCASE 0
 #define FMT_FLAGS_LCASE 1
@@ -18,7 +18,7 @@ void *mttstr_mem_rev(void *mem, size_t n);
 #define FMT_DEF_DEC { 10, '+', '-', ' ', FMT_FLAGS_LEFT_FILL | FMT_FLAGS_NULL_TERM, 0 }
 #define FMT_DEF_HEX { 16, 0, 0, ' ', FMT_FLAGS_MCASE | FMT_FLAGS_LEFT_FILL | FMT_FLAGS_NULL_TERM, 0 }
 
-#define IS_VAL_NEG(val) (val & (size_t)1 << (sizeof(val) * CHAR_BIT - 1))
+#define IS_VAL_NEG(val) (val & (std::size_t)1 << (sizeof(val) * CHAR_BIT - 1))
 
 class mttstr_fmt_t
 {
@@ -35,7 +35,7 @@ public:
 	uint8_t get_base() const;
 	void set_base(uint8_t base);
 
-	std::size_t ival_to_fstr(char *fstr, size_t ival) const;
+	std::size_t ival_to_fstr(char *fstr, std::size_t ival) const;
 
 	std::size_t fstr_to_ival(char *fstr, char **last) const;
 };
