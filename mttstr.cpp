6a186a14b9ee16ea6456834eb5458614bea372a3
Matteaz <79192782+gabrielematteaz@users.noreply.github.com>
2023-08-16 13:14:29 +0200

    new version

--- mttstr.cpp
+++ mttstr.cpp
@@ -2,20 +2,19 @@
 
 void *mttstr_mem_rev(void *mem, std::size_t n) noexcept
 {
-	if (mem != nullptr)
+	if (mem == nullptr) return nullptr;
+
+	char *m = (char *)mem, *mn = m + n;
+
+	while (m < mn)
 	{
-		char *m = (char *)mem, *mn = m + n;
+		mn--;
 
-		while (m < mn)
-		{
-			mn--;
+		char mc = *m;
 
-			char mc = *m;
-
-			*m = *mn;
-			m++;
-			*mn = mc;
-		}
+		*m = *mn;
+		m++;
+		*mn = mc;
 	}
 
 	return mem;
@@ -23,49 +22,28 @@ void *mttstr_mem_rev(void *mem, std::size_t n) noexcept
 
 void *mttstr_wmem_rev(void *wmem, std::size_t n) noexcept
 {
-	if (wmem != nullptr)
+	if (wmem == nullptr) return nullptr;
+
+	wchar_t *wm = (wchar_t *)wmem, *wmn = wm + n;
+
+	while (wm < wmn)
 	{
-		wchar_t *wm = (wchar_t *)wmem, *wmn = wm + n;
+		wmn--;
 
-		while (wm < wmn)
-		{
-			wmn--;
+		wchar_t wmc = *wm;
 
-			wchar_t wmc = *wm;
-
-			*wm = *wmn;
-			wm++;
-			*wmn = wmc;
-		}
+		*wm = *wmn;
+		wm++;
+		*wmn = wmc;
 	}
 
 	return wmem;
 }
 
-mttstr_fmt_t::mttstr_fmt_t() noexcept
-	: base(10), plus('+'), minus('-'), fill(' '), flags(FMT_FLAGS_LEFT_FILL | FMT_FLAGS_NULL_TERM), width(0)
-{
-
-}
-
-mttstr_fmt_t::mttstr_fmt_t(uint8_t base, char plus, char minus, char fill, uint8_t flags, uint8_t width) noexcept
-	: base(2 <= base && base <= 36 ? base : 10), plus(plus), minus(minus), fill(fill), flags(flags), width(width)
-{
-
-}
-
-uint8_t mttstr_fmt_t::get_base() const noexcept
-{
-	return base;
-}
-
-void mttstr_fmt_t::set_base(uint8_t base) noexcept
-{
-	this->base = 2 <= base && base <= 36 ? base : 10;
-}
-
 std::size_t mttstr_fmt_t::ival_to_fstr(char *fstr, std::size_t ival) const noexcept
 {
+	if (base < 2 || base > 36) return 0;
+
 	std::size_t len;
 
 	if (fstr == nullptr)
@@ -100,7 +78,7 @@ std::size_t mttstr_fmt_t::ival_to_fstr(char *fstr, std::size_t ival) const noexc
 
 		if (base > 10)
 		{
-			char a = flags & FMT_FLAGS_LCASE ? 87 : 55;
+			char a = fs & FMT_FS_LCASE ? 87 : 55;
 
 			do
 			{
@@ -123,7 +101,7 @@ std::size_t mttstr_fmt_t::ival_to_fstr(char *fstr, std::size_t ival) const noexc
 
 		char *fw;
 
-		if (flags & FMT_FLAGS_INT_FILL)
+		if (fs & FMT_FS_INT_FILL)
 		{
 			fw = fstr + width - 1;
 
@@ -152,7 +130,7 @@ std::size_t mttstr_fmt_t::ival_to_fstr(char *fstr, std::size_t ival) const noexc
 			len = f - fstr;
 			mttstr_mem_rev(fstr, len);
 		}
-		else if (flags & FMT_FLAGS_RIGHT_FILL)
+		else if (fs & FMT_FS_RIGHT_FILL)
 		{
 			if IS_VAL_NEG(i)
 			{
@@ -201,7 +179,7 @@ std::size_t mttstr_fmt_t::ival_to_fstr(char *fstr, std::size_t ival) const noexc
 			mttstr_mem_rev(fstr, len);
 		}
 
-		if (flags & FMT_FLAGS_NULL_TERM) *f = 0;
+		if (fs & FMT_FS_NULL_TERM) *f = 0;
 	}
 
 	return len;
@@ -211,11 +189,9 @@ std::string mttstr_fmt_t::ival_to_fstr(std::size_t ival) const
 {
 	std::size_t fstrsize = ival_to_fstr(nullptr, ival);
 
-	if (flags & FMT_FLAGS_NULL_TERM) fstrsize++;
+	if (fs & FMT_FS_NULL_TERM) fstrsize++;
 
-	fstrsize = fstrsize * sizeof(char);
-	
-	std::string fstr(fstrsize, 0);
+	std::string fstr(fstrsize * sizeof(char), 0);
 
 	ival_to_fstr(fstr.data(), ival);
 
@@ -224,155 +200,148 @@ std::string mttstr_fmt_t::ival_to_fstr(std::size_t ival) const
 
 std::size_t mttstr_fmt_t::fstr_to_ival(const char *fstr, const char **last) const noexcept
 {
-	if (fstr != nullptr)
+	if (fstr == nullptr || base < 2 || base > 36)
 	{
-		char fc = *fstr;
-		std::size_t s;
+		if (last != nullptr) *last = nullptr;
 
-		if (flags & FMT_FLAGS_INT_FILL)
+		return 0;
+	}
+
+	char fc = *fstr;
+	std::size_t s;
+
+	if (fs & FMT_FS_INT_FILL)
+	{
+		if (fc == minus)
 		{
-			if (fc == minus)
+			fstr++;
+			fc = *fstr;
+			s = -1;
+		}
+		else
+		{
+			if (fc == plus)
 			{
 				fstr++;
 				fc = *fstr;
-				s = -1;
-			}
-			else
-			{
-				if (fc == plus)
-				{
-					fstr++;
-					fc = *fstr;
-				}
-
-				s = 1;
 			}
 
+			s = 1;
+		}
+
+		while (fc == fill)
+		{
+			fstr++;
+			fc = *fstr;
+		}
+	}
+	else
+	{
+		if ((fs & FMT_FS_RIGHT_FILL) == 0x00)
+		{
 			while (fc == fill)
 			{
 				fstr++;
 				fc = *fstr;
 			}
 		}
-		else
+
+		if (fc == minus)
 		{
-			if ((flags & FMT_FLAGS_RIGHT_FILL) == 0)
-			{
-				while (fc == fill)
-				{
-					fstr++;
-					fc = *fstr;
-				}
-			}
-
-			if (fc == minus)
-			{
-				fstr++;
-				fc = *fstr;
-				s = -1;
-			}
-			else
-			{
-				if (fc == plus)
-				{
-					fstr++;
-					fc = *fstr;
-				}
-
-				s = 1;
-			}
-		}
-
-		std::size_t ival = 0;
-
-		if (base > 10)
-		{
-			if (flags & FMT_FLAGS_MCASE)
-			{
-				char umax = 'A' + base - 10, lmax = umax + 32;
-
-				while (1)
-				{
-					if ('0' <= fc && fc <= '9') ival = ival * base + fc - '0';
-					else if ('A' <= fc && fc < umax) ival = ival * base + fc - 55;
-					else if ('a' <= fc && fc < lmax) ival = ival * base + fc - 87;
-					else break;
-
-					fstr++;
-					fc = *fstr;
-				}
-			}
-			else
-			{
-				char min = flags & FMT_FLAGS_LCASE ? 'a' : 'A', max = min + base - 10;
-
-				while (1)
-				{
-					if ('0' <= fc && fc <= '9') ival = ival * base + fc - '0';
-					else if (min <= fc && fc < max) ival = ival * base + fc - min + 10;
-					else break;
-
-					fstr++;
-					fc = *fstr;
-				}
-			}
+			fstr++;
+			fc = *fstr;
+			s = -1;
 		}
 		else
 		{
-			char max = '0' + base;
-
-			while ('0' <= fc && fc < max)
+			if (fc == plus)
 			{
 				fstr++;
-				ival = ival * base + fc - '0';
 				fc = *fstr;
 			}
+
+			s = 1;
 		}
-
-		if (last != nullptr) *last = fstr;
-
-		return s * ival;
 	}
 
-	if (last != nullptr) *last = nullptr;
+	std::size_t ival = 0;
 
-	return 0;
-}
+	if (base > 10)
+	{
+		if (fs & FMT_FS_MCASE)
+		{
+			char umax = 'A' + base - 10, lmax = umax + 32;
 
-mttstr_wfmt_t::mttstr_wfmt_t() noexcept
-	: base(10), wplus(L'+'), wminus(L'-'), wfill(L' '), flags(FMT_FLAGS_LEFT_FILL | FMT_FLAGS_NULL_TERM), width(0)
-{
+			while (1)
+			{
+				if (fc < '0' || fc > '9')
+				{
+					if (fc < 'A' || umax <= fc)
+					{
+						if (fc < 'a' || lmax <= fc) break;
 
-}
+						ival = ival * base + fc - 87;
+					}
+					else ival = ival * base + fc - 55;
+				}
+				else ival = ival * base + fc - '0';
 
-mttstr_wfmt_t::mttstr_wfmt_t(uint8_t base, wchar_t wplus, wchar_t wminus, wchar_t wfill, uint8_t flags, uint8_t width) noexcept
-	: base(2 <= base && base <= 36 ? base : 10), wplus(wplus), wminus(wminus), wfill(wfill), flags(flags), width(width)
-{
+				fstr++;
+				fc = *fstr;
+			}
+		}
+		else
+		{
+			char min = fs & FMT_FS_LCASE ? 'a' : 'A', max = min + base - 10;
 
-}
+			while (1)
+			{
+				if (fc < '0' || fc > '9')
+				{
+					if (fc < min || max <= fc) break;
 
-uint8_t mttstr_wfmt_t::get_base() const noexcept
-{
-	return base;
-}
+					ival = ival * base + fc - min + 10;
+				}
+				else ival = ival * base + fc - '0';
 
-void mttstr_wfmt_t::set_base(uint8_t base) noexcept
-{
-	this->base = 2 <= base && base <= 36 ? base : 10;
+				fstr++;
+				fc = *fstr;
+			}
+		}
+	}
+	else
+	{
+		char max = '0' + base;
+
+		while (1)
+		{
+			if (fc < '0' || max <= fc) break;
+
+			fstr++;
+			ival = ival * base + fc - '0';
+			fc = *fstr;
+		}
+	}
+
+	if (last != nullptr) *last = fstr;
+
+	return s * ival;
 }
 
 std::size_t mttstr_wfmt_t::ival_to_wfstr(wchar_t *wfstr, std::size_t ival) const noexcept
 {
+if (base < 2 || base > 36) return 0;
+
 	std::size_t len;
 
 	if (wfstr == nullptr)
 	{
-		if (wminus && IS_VAL_NEG(ival))
+		if (minus && IS_VAL_NEG(ival))
 		{
 			ival = -ival;
 			len = 1;
 		}
-		else len = wplus ? 1 : 0;
+		else len = plus ? 1 : 0;
 
 		do
 		{
@@ -386,7 +355,7 @@ std::size_t mttstr_wfmt_t::ival_to_wfstr(wchar_t *wfstr, std::size_t ival) const
 	{
 		std::size_t i;
 
-		if (wminus && IS_VAL_NEG(ival))
+		if (minus && IS_VAL_NEG(ival))
 		{
 			i = ival;
 			ival = -ival;
@@ -397,14 +366,14 @@ std::size_t mttstr_wfmt_t::ival_to_wfstr(wchar_t *wfstr, std::size_t ival) const
 
 		if (base > 10)
 		{
-			wchar_t wa = flags & FMT_FLAGS_LCASE ? 87 : 55;
+			wchar_t wa = fs & FMT_FS_LCASE ? 87 : 55;
 
 			do
 			{
 				wchar_t wrem = ival % base;
 
 				ival /= base;
-				*wf = (wrem < 10 ? L'0' : wa) + wrem;
+				*wf = (wrem < 10 ? '0' : wa) + wrem;
 				wf++;
 			} while (ival);
 		}
@@ -412,7 +381,7 @@ std::size_t mttstr_wfmt_t::ival_to_wfstr(wchar_t *wfstr, std::size_t ival) const
 		{
 			do
 			{
-				*wf = L'0' + ival % base;
+				*wf = '0' + ival % base;
 				ival /= base;
 				wf++;
 			} while (ival);
@@ -420,45 +389,45 @@ std::size_t mttstr_wfmt_t::ival_to_wfstr(wchar_t *wfstr, std::size_t ival) const
 
 		wchar_t *wfw;
 
-		if (flags & FMT_FLAGS_INT_FILL)
+		if (fs & FMT_FS_INT_FILL)
 		{
 			wfw = wfstr + width - 1;
 
 			while (wf < wfw)
 			{
-				*wf = wfill;
+				*wf = fill;
 				wf++;
 			}
 
 			if IS_VAL_NEG(i)
 			{
-				*wf = wminus;
+				*wf = minus;
 				wf++;
 			}
-			else if (wplus)
+			else if (plus)
 			{
-				*wf = wplus;
+				*wf = plus;
 				wf++;
 			}
 			else if (wf == wfw)
 			{
-				*wf = wfill;
+				*wf = fill;
 				wf++;
 			}
 
 			len = wf - wfstr;
 			mttstr_wmem_rev(wfstr, len);
 		}
-		else if (flags & FMT_FLAGS_RIGHT_FILL)
+		else if (fs & FMT_FS_RIGHT_FILL)
 		{
 			if IS_VAL_NEG(i)
 			{
-				*wf = wminus;
+				*wf = minus;
 				wf++;
 			}
-			else if (wplus)
+			else if (plus)
 			{
-				*wf = wplus;
+				*wf = plus;
 				wf++;
 			}
 
@@ -467,7 +436,7 @@ std::size_t mttstr_wfmt_t::ival_to_wfstr(wchar_t *wfstr, std::size_t ival) const
 
 			while (wf < wfw)
 			{
-				*wf = wfill;
+				*wf = fill;
 				wf++;
 			}
 
@@ -477,12 +446,12 @@ std::size_t mttstr_wfmt_t::ival_to_wfstr(wchar_t *wfstr, std::size_t ival) const
 		{
 			if IS_VAL_NEG(i)
 			{
-				*wf = wminus;
+				*wf = minus;
 				wf++;
 			}
-			else if (wplus)
+			else if (plus)
 			{
-				*wf = wplus;
+				*wf = plus;
 				wf++;
 			}
 
@@ -490,7 +459,7 @@ std::size_t mttstr_wfmt_t::ival_to_wfstr(wchar_t *wfstr, std::size_t ival) const
 
 			while (wf < wfw)
 			{
-				*wf = wfill;
+				*wf = fill;
 				wf++;
 			}
 
@@ -498,7 +467,7 @@ std::size_t mttstr_wfmt_t::ival_to_wfstr(wchar_t *wfstr, std::size_t ival) const
 			mttstr_wmem_rev(wfstr, len);
 		}
 
-		if (flags & FMT_FLAGS_NULL_TERM) *wf = 0;
+		if (fs & FMT_FS_NULL_TERM) *wf = 0;
 	}
 
 	return len;
@@ -508,11 +477,9 @@ std::wstring mttstr_wfmt_t::ival_to_wfstr(std::size_t ival) const
 {
 	std::size_t wfstrsize = ival_to_wfstr(nullptr, ival);
 
-	if (flags & FMT_FLAGS_NULL_TERM) wfstrsize++;
+	if (fs & FMT_FS_NULL_TERM) wfstrsize++;
 
-	wfstrsize = wfstrsize * sizeof(wchar_t);
-	
-	std::wstring wfstr(wfstrsize, 0);
+	std::wstring wfstr(wfstrsize * sizeof(wchar_t), 0);
 
 	ival_to_wfstr(wfstr.data(), ival);
 
@@ -521,117 +488,130 @@ std::wstring mttstr_wfmt_t::ival_to_wfstr(std::size_t ival) const
 
 std::size_t mttstr_wfmt_t::wfstr_to_ival(const wchar_t *wfstr, const wchar_t **wlast) const noexcept
 {
-	if (wfstr != nullptr)
+	if (wfstr == nullptr || base < 2 || base > 36)
 	{
-		wchar_t wfc = *wfstr;
-		std::size_t s;
+		if (wlast != nullptr) *wlast = nullptr;
 
-		if (flags & FMT_FLAGS_INT_FILL)
-		{
-			if (wfc == wminus)
-			{
-				wfstr++;
-				wfc = *wfstr;
-				s = -1;
-			}
-			else
-			{
-				if (wfc == wplus)
-				{
-					wfstr++;
-					wfc = *wfstr;
-				}
-
-				s = 1;
-			}
-
-			while (wfc == wfill)
-			{
-				wfstr++;
-				wfc = *wfstr;
-			}
-		}
-		else
-		{
-			if ((flags & FMT_FLAGS_RIGHT_FILL) == 0)
-			{
-				while (wfc == wfill)
-				{
-					wfstr++;
-					wfc = *wfstr;
-				}
-			}
-
-			if (wfc == wminus)
-			{
-				wfstr++;
-				wfc = *wfstr;
-				s = -1;
-			}
-			else
-			{
-				if (wfc == wplus)
-				{
-					wfstr++;
-					wfc = *wfstr;
-				}
-
-				s = 1;
-			}
-		}
-
-		std::size_t ival = 0;
-
-		if (base > 10)
-		{
-			if (flags & FMT_FLAGS_MCASE)
-			{
-				wchar_t wumax = L'A' + base - 10, wlmax = wumax + 32;
-
-				while (1)
-				{
-					if (L'0' <= wfc && wfc <= L'9') ival = ival * base + wfc - L'0';
-					else if (L'A' <= wfc && wfc < wumax) ival = ival * base + wfc - 55;
-					else if (L'a' <= wfc && wfc < wlmax) ival = ival * base + wfc - 87;
-					else break;
-
-					wfstr++;
-					wfc = *wfstr;
-				}
-			}
-			else
-			{
-				wchar_t wmin = flags & FMT_FLAGS_LCASE ? L'a' : L'A', wmax = wmin + base - 10;
-
-				while (1)
-				{
-					if (L'0' <= wfc && wfc <= L'9') ival = ival * base + wfc - L'0';
-					else if (wmin <= wfc && wfc < wmax) ival = ival * base + wfc - wmin + 10;
-					else break;
-
-					wfstr++;
-					wfc = *wfstr;
-				}
-			}
-		}
-		else
-		{
-			wchar_t wmax = L'0' + base;
-
-			while (L'0' <= wfc && wfc < wmax)
-			{
-				wfstr++;
-				ival = ival * base + wfc - L'0';
-				wfc = *wfstr;
-			}
-		}
-
-		if (wlast != nullptr) *wlast = wfstr;
-
-		return s * ival;
+		return 0;
 	}
 
-	if (wlast != nullptr) *wlast = nullptr;
+	wchar_t wfc = *wfstr;
+	std::size_t s;
 
-	return 0;
+	if (fs & FMT_FS_INT_FILL)
+	{
+		if (wfc == minus)
+		{
+			wfstr++;
+			wfc = *wfstr;
+			s = -1;
+		}
+		else
+		{
+			if (wfc == plus)
+			{
+				wfstr++;
+				wfc = *wfstr;
+			}
+
+			s = 1;
+		}
+
+		while (wfc == fill)
+		{
+			wfstr++;
+			wfc = *wfstr;
+		}
+	}
+	else
+	{
+		if ((fs & FMT_FS_RIGHT_FILL) == 0x00)
+		{
+			while (wfc == fill)
+			{
+				wfstr++;
+				wfc = *wfstr;
+			}
+		}
+
+		if (wfc == minus)
+		{
+			wfstr++;
+			wfc = *wfstr;
+			s = -1;
+		}
+		else
+		{
+			if (wfc == plus)
+			{
+				wfstr++;
+				wfc = *wfstr;
+			}
+
+			s = 1;
+		}
+	}
+
+	std::size_t ival = 0;
+
+	if (base > 10)
+	{
+		if (fs & FMT_FS_MCASE)
+		{
+			wchar_t wumax = L'A' + base - 10, wlmax = wumax + 32;
+
+			while (1)
+			{
+				if (wfc < L'0' || wfc > L'9')
+				{
+					if (wfc < L'A' || wumax <= wfc)
+					{
+						if (wfc < L'a' || wlmax <= wfc) break;
+
+						ival = ival * base + wfc - 87;
+					}
+					else ival = ival * base + wfc - 55;
+				}
+				else ival = ival * base + wfc - L'0';
+
+				wfstr++;
+				wfc = *wfstr;
+			}
+		}
+		else
+		{
+			wchar_t wmin = fs & FMT_FS_LCASE ? L'a' : L'A', wmax = wmin + base - 10;
+
+			while (1)
+			{
+				if (wfc < L'0' || wfc > L'9')
+				{
+					if (wfc < wmin || wmax <= wfc) break;
+
+					ival = ival * base + wfc - wmin + 10;
+				}
+				else ival = ival * base + wfc - L'0';
+
+				wfstr++;
+				wfc = *wfstr;
+			}
+		}
+	}
+	else
+	{
+		wchar_t wmax = L'0' + base;
+
+		while (1)
+		{
+			if (wfc < L'0' || wmax <= wfc) break;
+
+			wfstr++;
+			ival = ival * base + wfc - L'0';
+			wfc = *wfstr;
+		}
+	}
+
+	if (wlast != nullptr) *wlast = wfstr;
+
+	return s * ival;
 }
--- mttstr.hpp
+++ mttstr.hpp
@@ -3,38 +3,57 @@
 
 #include <string>
 
-void *mttstr_mem_rev(void *mem, std::size_t n) noexcept;
-void *mttstr_wmem_rev(void *wmem, std::size_t n) noexcept;
+#define FMT_FS_UCASE 0x00
+#define FMT_FS_LCASE 0x01
+#define FMT_FS_MCASE 0x02
+#define FMT_FS_LEFT_FILL 0x00
+#define FMT_FS_INT_FILL 0x04
+#define FMT_FS_RIGHT_FILL 0x08
+#define FMT_FS_DO_NOT_NULL_TERM 0x00
+#define FMT_FS_NULL_TERM 0x10
 
-#define FMT_FLAGS_UCASE 0
-#define FMT_FLAGS_LCASE 1
-#define FMT_FLAGS_MCASE 2
-#define FMT_FLAGS_LEFT_FILL 0
-#define FMT_FLAGS_INT_FILL 4
-#define FMT_FLAGS_RIGHT_FILL 8
-#define FMT_FLAGS_DO_NOT_NULL_TERM 0
-#define FMT_FLAGS_NULL_TERM 16
-#define FMT_DEF_DEC { 10, '+', '-', ' ', FMT_FLAGS_LEFT_FILL | FMT_FLAGS_NULL_TERM, 0 }
-#define FMT_DEF_HEX { 16, 0, 0, ' ', FMT_FLAGS_MCASE | FMT_FLAGS_LEFT_FILL | FMT_FLAGS_NULL_TERM, 0 }
-#define WFMT_DEF_DEC { 10, L'+', L'-', L' ', FMT_FLAGS_LEFT_FILL | FMT_FLAGS_NULL_TERM, 0 }
-#define WFMT_DEF_HEX { 16, 0, 0, L' ', FMT_FLAGS_MCASE | FMT_FLAGS_LEFT_FILL | FMT_FLAGS_NULL_TERM, 0 }
+#define FMT_DEF_HEX { 0, 0, ' ', 16, FMT_FS_MCASE | FMT_FS_LEFT_FILL | FMT_FS_NULL_TERM, 0 }
+
+#define WFMT_DEF_HEX { 0, 0, L' ', 16, FMT_FS_MCASE | FMT_FS_LEFT_FILL | FMT_FS_NULL_TERM, 0 }
 
 #define IS_VAL_NEG(val) (val & (std::size_t)1 << (sizeof(val) * CHAR_BIT - 1))
 
+void *mttstr_mem_rev(void *mem, std::size_t n) noexcept;
+void *mttstr_wmem_rev(void *wmem, std::size_t n) noexcept;
+
 class mttstr_fmt_t
 {
-	uint8_t base;
-
 public:
 	char plus, minus, fill;
-	uint8_t flags;
-	uint8_t width;
 
-	mttstr_fmt_t() noexcept;
-	mttstr_fmt_t(uint8_t base, char plus, char minus, char fill, uint8_t flags, uint8_t width) noexcept;
+private:
+	int base;
 
-	uint8_t get_base() const noexcept;
-	void set_base(uint8_t base) noexcept;
+public:
+	int fs;
+	std::size_t width;
+
+	inline mttstr_fmt_t() noexcept
+		: plus('+'), minus('-'), fill(' '), base(10), fs(FMT_FS_LEFT_FILL | FMT_FS_NULL_TERM), width(0)
+	{
+
+	}
+
+	inline mttstr_fmt_t(char plus, char minus, char fill, int base, int fs, std::size_t width) noexcept
+		: plus(plus), minus(minus), fill(fill), base(base < 2 || base > 36 ? 10 : base), fs(fs), width(width)
+	{
+
+	}
+
+	inline int get_base() const noexcept
+	{
+		return base;
+	}
+
+	inline void set_base(int base) noexcept
+	{
+		this->base = base < 2 || base > 36 ? 10 : base;
+	}
 
 	std::size_t ival_to_fstr(char *fstr, std::size_t ival) const noexcept;
 	std::string ival_to_fstr(std::size_t ival) const;
@@ -44,18 +63,37 @@ public:
 
 class mttstr_wfmt_t
 {
-	uint8_t base;
+public:
+	wchar_t plus, minus, fill;
+
+private:
+	int base;
 
 public:
-	wchar_t wplus, wminus, wfill;
-	uint8_t flags;
-	uint8_t width;
+	int fs;
+	std::size_t width;
 
-	mttstr_wfmt_t() noexcept;
-	mttstr_wfmt_t(uint8_t base, wchar_t wplus, wchar_t wminus, wchar_t wfill, uint8_t flags, uint8_t width) noexcept;
+	inline mttstr_wfmt_t() noexcept
+		: plus(L'+'), minus(L'-'), fill(L' '), base(10), fs(FMT_FS_LEFT_FILL | FMT_FS_NULL_TERM), width(0)
+	{
 
-	uint8_t get_base() const noexcept;
-	void set_base(uint8_t base) noexcept;
+	}
+
+	inline mttstr_wfmt_t(wchar_t plus, wchar_t minus, wchar_t fill, int base, int fs, std::size_t width) noexcept
+		: plus(plus), minus(minus), fill(fill), base(base < 2 || base > 36 ? 10 : base), fs(fs), width(width)
+	{
+
+	}
+
+	inline int get_base() const noexcept
+	{
+		return base;
+	}
+
+	inline void set_base(int base) noexcept
+	{
+		this->base = base < 2 || base > 36 ? 10 : base;
+	}
 
 	std::size_t ival_to_wfstr(wchar_t *wfstr, std::size_t ival) const noexcept;
 	std::wstring ival_to_wfstr(std::size_t ival) const;
